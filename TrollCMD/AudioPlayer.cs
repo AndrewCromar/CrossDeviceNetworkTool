3b4acd35b67c097d3be30ff68273e9aa8afe603e
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-08-15 16:14:12 -0700

    feat(commands): added some new commands

---
+++ TrollCMD/AudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using WMPLib;
+using System.IO;
+
+public static class AudioPlayer
+{
+    private static WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+    public static void PlayAudio(byte[] audioData)
+    {
+        if (audioData == null || audioData.Length == 0)
+        {
+            throw new ArgumentException("Audio data cannot be null or empty", nameof(audioData));
+        }
+
+        // Save byte array to a temporary file
+        string tempFilePath = Path.GetTempFileName() + ".mp3";
+        File.WriteAllBytes(tempFilePath, audioData);
+
+        player.URL = tempFilePath;
+        player.controls.play();
+
+        // Optionally delete the temporary file after playback
+        // player.PlayStateChange += (state) =>
+        // {
+        //     if (state == (int)WMPPlayState.wmppsStopped)
+        //     {
+        //         File.Delete(tempFilePath);
+        //     }
+        // };
+    }
+}
